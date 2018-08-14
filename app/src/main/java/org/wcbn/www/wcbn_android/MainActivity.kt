1ee8cb2a5005e6bebc1925c311f33c3e3502af79
Alex Okros <akros@umich.edu>
2018-08-14 15:30:04 -0400

    Removed Settings button and suppressed view: View warnings

--- app/src/main/java/org/wcbn/www/wcbn_android/MainActivity.kt
+++ app/src/main/java/org/wcbn/www/wcbn_android/MainActivity.kt
@@ -1,28 +1,35 @@
 package org.wcbn.www.wcbn_android
 
+
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.RadioGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import org.jsoup.Jsoup
+
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private var playing: Boolean = false
     private var live: Boolean = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(
+        //setupPermissions()
+
+
+            val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
@@ -32,27 +39,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         displayFragment(-1)
     }
 
+
+
+    //Begin Permissions
+
+    //End Permissions
+
+
+
+
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
         }
     }
 
@@ -72,7 +72,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 HomeFragment()
             }
         }
-
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.relativeLayout, fragment)
@@ -90,11 +89,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
 
 
+
     //Buttons!
-    fun buttonPlayPause(view: View) {
+    fun buttonPlayPause(@Suppress("UNUSED_PARAMETER") view: View) {
         if (!playing) {
             playing = true
             Toast.makeText(this, "play clicky", Toast.LENGTH_SHORT).show()
+            /*
+            var doc = Jsoup.connect("http://app.wcbn.org/").get().run {
+                select("div.rc").forEachIndexed { index, element ->
+                    val titleAnchor = element.select("h3 a")
+                    val title = titleAnchor.text()
+                    val url = titleAnchor.attr("href")
+
+                    println("$index. $title ($url)")
+                }
+            }
+            */
+
         }
         else {
             playing = false
@@ -105,7 +117,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    fun buttonLive(view: View) {
+
+    fun buttonLive(@Suppress("UNUSED_PARAMETER") view: View) {
         if (!live) {
             live = true
             Toast.makeText(this, "live clicky", Toast.LENGTH_SHORT).show()
